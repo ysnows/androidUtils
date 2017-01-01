8528e872e706f9e078f8ff8a9947ea97b2b9d890
xianguangjin <1447360366@qq.com>
2017-01-01 10:06:05 +0800

    first

---
+++ WindowUtils.java
@@ -0,0 +1,116 @@
+package ysnows.myapplication;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.view.Gravity;
+import android.view.WindowManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xianguangjin on 2017/1/1.
+ */
+
+public class WindowUtils {
+    private static WindowManager mWindowManager;
+    private static FloatWindowSmallView smallWindow;
+    private static WindowManager.LayoutParams smallWindowParams;
+    private static float float_width;
+
+    /**
+     * 创建一个小悬浮窗。初始位置为屏幕的右部中间位置。
+     *
+     * @param context 必须为应用程序的Context.
+     */
+    public static void createSmallWindow(Context context) {
+        WindowManager windowManager = getWindowManager(context);
+        int screenWidth = windowManager.getDefaultDisplay().getWidth();
+        int screenHeight = windowManager.getDefaultDisplay().getHeight();
+        float_width = context.getResources().getDimensionPixelSize(R.dimen.float_width);
+        if (smallWindow == null) {
+            smallWindow = new FloatWindowSmallView(context);
+            if (smallWindowParams == null) {
+                smallWindowParams = new WindowManager.LayoutParams();
+                smallWindowParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+                smallWindowParams.format = PixelFormat.RGBA_8888;
+                smallWindowParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                        | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+                smallWindowParams.gravity = Gravity.LEFT | Gravity.TOP;
+                smallWindowParams.width = (int) float_width;
+                smallWindowParams.height = (int) float_width;
+                smallWindowParams.x = screenWidth;
+                smallWindowParams.y = screenHeight / 2;
+            }
+            smallWindow.setParams(smallWindowParams);
+            windowManager.addView(smallWindow, smallWindowParams);
+        }
+    }
+
+
+    /**
+     * 如果WindowManager还未创建，则创建一个新的WindowManager返回。否则返回当前已创建的WindowManager。
+     *
+     * @param context 必须为应用程序的Context.
+     * @return WindowManager的实例，用于控制在屏幕上添加或移除悬浮窗。
+     */
+    private static WindowManager getWindowManager(Context context) {
+        if (mWindowManager == null) {
+            mWindowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        }
+        return mWindowManager;
+    }
+
+
+    /**
+     * @param activity 检查覆盖其他应用权限
+     */
+    public static void checkDrawOverlay(Activity activity) {
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!Settings.canDrawOverlays(activity)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + activity.getPackageName()));
+                activity.startActivityForResult(intent, 1234);
+            }
+        }
+    }
+
+
+    /**
+     * 判断当前界面是否是桌面
+     */
+    public static boolean isHome(Context context) {
+        ActivityManager mActivityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> rti = mActivityManager.getRunningTasks(1);
+        return getHomes(context).contains(rti.get(0).topActivity.getPackageName());
+    }
+
+    /**
+     * 获得属于桌面的应用的应用包名称
+     *
+     * @return 返回包含所有包名的字符串列表
+     */
+    public static List<String> getHomes(Context context) {
+        List<String> names = new ArrayList<String>();
+        PackageManager packageManager = context.getPackageManager();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+
+        List<ResolveInfo> resolveInfo = packageManager.queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+
+        for (ResolveInfo ri : resolveInfo) {
+            names.add(ri.activityInfo.packageName);
+        }
+        return names;
+    }
+}
